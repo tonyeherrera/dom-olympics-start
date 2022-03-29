d52dc17870350812979f1992dad408c1829d7e90
tonyeherrera <74445648+tonyeherrera@users.noreply.github.com>
2022-03-28 23:19:56 -0400

    fixes to css change via dom manip

--- index.js
+++ index.js
@@ -22,47 +22,70 @@ clear.addEventListener("click", function clearMessages(){
 
 
 
-var left = document.getElementsByClassName("left")
+var left = document.querySelectorAll(".left")
 var right = document.getElementsByClassName("right")
 
 document.getElementById("theme-drop-down").addEventListener("change", function theme(){
     var dropDown = document.getElementById("theme-drop-down").value
-    console.log(dropDown)
-if ( dropDown === "theme-one"){
-    for (var i = 0; i < left.length; i++){
-    left[i].style.backgroundColor = "burlywood";
-    left[i].style.color = "black";
-    right[i].style.backgroundColor = "lightblue";
-    right[i].style.color = "black";
-}}if ( dropDown === "theme-two"){
-    for (var i=0; i< left.length; i++){
-    left[i].style.backgroundColor = "black";
-    left[i].style.color = "white";
-    right[i].style.backgroundColor = "maroon";
-    right[i].style.color = "white";
+    var leftStyle = document.styleSheets[0].cssRules[6].style;
+    var rightStyle = document.styleSheets[0].cssRules[5].style;
     
-}}if ( dropDown === "theme-three"){
-    for (var i=0; i< left.length; i++){
-    left[i].style.backgroundColor = "forestgreen";
-    left[i].style.color = "silver";
-    right[i].style.backgroundColor = "goldenrod";
-    right[i].style.color = "gainsboro";
-}}    
+if ( dropDown === "theme-one"){
+    leftStyle.backgroundColor = "burlywood";
+    leftStyle.color = "black";
+    rightStyle.backgroundColor = "lightblue";
+    rightStyle.color = "black";
+    // for (var i = 0; i < left.length; i++){
+    // left[i].style.backgroundColor = "burlywood";
+    // left[i].style.color = "black";
+    // right[i].style.backgroundColor = "lightblue";
+    // right[i].style.color = "black";}
+}if ( dropDown === "theme-two"){
+    leftStyle.backgroundColor = "black";
+    leftStyle.color = "white";
+    rightStyle.backgroundColor = "maroon";
+    rightStyle.color = "white";
+    // for (var i=0; i< left.length; i++){
+    // left[i].style.backgroundColor = "black";
+    // left[i].style.color = "white";
+    // right[i].style.backgroundColor = "maroon";
+    // right[i].style.color = "white";}
+    
+}if ( dropDown === "theme-three"){
+    leftStyle.backgroundColor = "forestgreen";
+    leftStyle.color = "silver";
+    rightStyle.backgroundColor = "goldenrod";
+    rightStyle.color = "gainsboro";
+    // for (var i=0; i< left.length; i++){
+    // left[i].style.backgroundColor = "forestgreen";
+    // left[i].style.color = "silver";
+    // right[i].style.backgroundColor = "goldenrod";
+    // right[i].style.color = "gainsboro";}
+}    
 if ( dropDown === "theme-four"){
-    for (var i=0; i< left.length; i++){
-    left[i].style.backgroundColor = "pink";
-    left[i].style.color = "darkblue";
-    right[i].style.backgroundColor = "purple";
-    right[i].style.color = "lightblue";
-}}   
+    console.log(leftStyle);
+    leftStyle.backgroundColor = "pink";
+    leftStyle.color = "darkblue";
+    rightStyle.backgroundColor = "purple";
+    rightStyle.color = "lightblue";
+        // for (var i=0; i< left.length; i++){
+    // left[i].style.backgroundColor = "pink";
+    // left[i].style.color = "darkblue";
+    // right[i].style.backgroundColor = "purple";
+    // right[i].style.color = "lightblue";}
+}  
 if ( dropDown === "theme-five"){
-        for (var i=0; i< left.length; i++){
-        left[i].style.backgroundColor = "black";
-        left[i].style.color = "white";
-        right[i].style.backgroundColor = "white";
-        right[i].style.color = "black";
+    leftStyle.backgroundColor = "black";
+    leftStyle.color = "white";
+    rightStyle.backgroundColor = "white";
+    rightStyle.color = "black";
+        // for (var i=0; i< left.length; i++){
+        // left[i].style.backgroundColor = "black";
+        // left[i].style.color = "white";
+        // right[i].style.backgroundColor = "white";
+        // right[i].style.color = "black";}
             
-}}});
+}});
 
 var whoSaidIt = "left";
 document.getElementById("send").addEventListener("click",function conversation(){
